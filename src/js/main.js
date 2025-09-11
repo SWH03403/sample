008c12c30a1e1d5e1c3ccdb56afed7600da5f401
SWH03403 <106224482@student.swin.edu.au>
2025-09-11 23:54:01 +0700

    refactor(js): generic showcase initializer

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,21 @@
-const counterMaps = [x => 0, x => x + 1, x => x + 2, x => x + 5, x => x * 2];
-function initCounter() {
-	const counter = document.getElementById("section-counter");
-	const value = counter.getElementsByClassName("value")[0];
-	const buttonsCollection = counter.getElementsByClassName("controls")[0].children;
+function initShowcase(showcase) {
+	const container = document.getElementById(`sc-${showcase.name}`);
+	const value = container.getElementsByClassName("value")[0];
+	const buttonsCollection = container.getElementsByClassName("controls")[0].children;
 	const buttons = Array.from(buttonsCollection);
 	buttons.forEach((button, idx) => button.addEventListener("click", function() {
-		const init = parseInt(value.innerText);
-		value.innerText = counterMaps[idx](init);
+		showcase.run(value, idx);
 	}));
 }
 
-document.addEventListener("DOMContentLoaded", initCounter);
+const counterMaps = [x => 0, x => x + 1, x => x + 2, x => x + 5, x => x * 2];
+
+const showcases = [
+	{
+		name: "counter",
+		run: (value, idx) => value.innerText = counterMaps[idx](parseInt(value.innerText)),
+	},
+];
+showcases.forEach(s => {
+	document.addEventListener("DOMContentLoaded", function() { initShowcase(s); });
+});
